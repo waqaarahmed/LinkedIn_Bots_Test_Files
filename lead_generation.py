c9be39aa04ca6514eca302055ac25fc05bf5070b
Waqar Ahmed <ahmedwaqar309@gmail.com>
2024-09-11 23:38:38 +0500

    Update lead_generation.py

--- lead_generation.py
+++ lead_generation.py
@@ -1,1 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, ElementClickInterceptedException
+from time import sleep
+import random
 
+#Loading Chromedriver
+driver = webdriver.Chrome()
+
+# Navigate to LinkedIn login page
+driver.get("https://www.linkedin.com/login")
+sleep(5)
+
+# Log in to LinkedIn
+username = driver.find_element(By.ID, "username")
+password = driver.find_element(By.ID, "password")
+
+# Enter your LinkedIn credentials here
+username.send_keys("your email")
+password.send_keys("your password")
+password.send_keys(Keys.RETURN)
+
+# Waiting for the page to load
+sleep(random.uniform(2, 5))
