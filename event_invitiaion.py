0aec21a609b7d9afabea6cf9aa672b922a0eb80a
Waqar Ahmed <ahmedwaqar309@gmail.com>
2024-09-19 23:40:56 +0500

    Update event_invitiaion.py

--- event_invitiaion.py
+++ event_invitiaion.py
@@ -1,1 +1,10 @@
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, ElementClickInterceptedException
+from time import sleep
+import random
+
